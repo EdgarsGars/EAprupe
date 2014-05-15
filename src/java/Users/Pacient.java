2c677db57a2daac5ed8e67db14b44abb74524d11
EdgarsGars <edgars.garsneks@gmail.com>
2014-05-15 18:11:14 +0300

    Web

--- src/java/Users/Pacient.java
+++ src/java/Users/Pacient.java
@@ -7,9 +7,56 @@
 package Users;
 
 /**
- * 
+ * Pacient object 
  * @author Edgar
  */
 public class Pacient {
+    private final String id;
+    private final String name;
+    private final String surname;
+    private final String number;
+    private final String email;
+    private final String address;
+    private final String familyDoctor;
+
+    public Pacient(String id, String name, String surname, String number, String email, String address, String familyDoctor) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.number = number;
+        this.email = email;
+        this.address = address;
+        this.familyDoctor = familyDoctor;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getFamilyDoctor() {
+        return familyDoctor;
+    }
+    
+    
     
 }
